4cb6962d33e9b956c7f543463f114c6f3e4fd9f7
Siqi Chen <chensiqi911@gmail.com>
2025-10-18 17:02:17 -0400

    Finish catalog feature

---
+++ frontend/src/Products/data.tsx
@@ -0,0 +1,35 @@
+export default {
+    products: [{
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+        name: 'Shirt',
+        imageUrl: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numberOfReviews: 10,
+    },
+    {
+
+      name: 'Pants',
+      imageUrl: '/images/d2.jpg',
+      price: 80,
+      brand: 'Adidas',
+      rating: 4.2,
+      numberOfReviews: 8,
+    },
+    {
+      name: 'Hat',
+      imageUrl: '/images/d3.jpg',
+      price: 25,
+      brand: 'Puma',
+      rating: 4.7,
+      numberOfReviews: 14,
+    }]
+};
